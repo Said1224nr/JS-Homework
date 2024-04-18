310c1ba145dd27ba04e590530518d37f9ec0ba28
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-18 20:53:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// console.log(Math.round(Math.random() * 100));
+
+// let num = 5
+// console.log(Math.pow(num, 2));
+
+let Random = +prompt('Random son kiriting')
+
+let Kop = Random * 2
+
+let Kam = Random / 2
+
+alert(`${Random}ni 2 ga Kopaytirsa ${Kop}ga teng\n${Random}ni 2 ga Bolsa ${Kam}ga teng`)
+
